bc327e5976759e3eb2bbae6b96d6dd22cbf83044
unknown <zachary@thelamears.com>
2023-01-22 20:32:11 -0600

    ok

--- hello_git.cpp
+++ hello_git.cpp
@@ -2,10 +2,12 @@
 #include <fstream>
 
 using namespace std;
-
+This is will error i swear
 int main ()
 {
 	cout << "\nhello git\n";
 	cout << "Creating a merge conflict\n";
+	give me an error please
+	
 	return 0;
 }
